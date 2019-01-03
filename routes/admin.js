9777fbaccb756a2e974cdc7662dd9d3c537998d2
Ria Bhatia <riabhatia24@gmail.com>
2019-01-03 19:42:00 +0530

    Shopping App Pages + CSS

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,19 @@
 const express = require('express');
-
+const path = require('path');
+const rootDir = require('../util/path'); 
 const router = express.Router();
 
-router.use('/add-product', (req, res, next)=> {
+router.get('/add-product', (req, res, next)=> {
     console.log("In the middleleware");
-    res.send('<form action ="/product" method="POST"><input type text="text" name="title"><button type ="submit">Set</button></form>');
+    res.sendFile(path.join(rootDir,'views', 'add-product.html'));
      
    });
   
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/'); 
      
-     
+      
    });
 
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,12 @@
 const express = require('express');
-
+const path = require('path');
+const rootDir = require('../util/path'); 
 const router = express.Router();
 
 
 router.get('/', (req, res, next) => {
     console.log("In the middleleware2");
-    res.send('<html>Hello</html>');  
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));  
    });
 
 
